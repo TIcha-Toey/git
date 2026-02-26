6a1255a947c6f67e339f69dbfce355abe7bf7664
TIcha.Ub <tichaz12@gmail.com>
2026-02-26 23:57:14 +0700

    feature 2: 3 commit

--- feature2/add_customer.ts
+++ feature2/add_customer.ts
@@ -1,5 +1,9 @@
-const customer = {
+const customers = [{
     id: 1,
-    name: "John Doe",
-    email: "john.doe@example.com"
-}
+    name: 'John Doe',
+    email: 'john.doe@example.com'
+},{
+    id: 2,
+    name: 'Jane Smith',
+    email: 'jane.smith@example.com'
+}]
